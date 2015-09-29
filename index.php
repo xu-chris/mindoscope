f2cbe19818e6dce60cf300568a64193124f7147d
Christian Reichel <c.reichel@me.com>
2015-09-29 13:07:26 +0200

    Fixing the user viewport scale

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 
     <!--iOS -->
     <meta name="apple-mobile-web-app-title" content="Mind-o-scope">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <link rel="apple-touch-icon-precomposed" href="assets/img/app-icons/iTunesArtwork.png">
